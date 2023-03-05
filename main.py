fb25b45550af6429abf534d71b80ebdaec5805ec
Berlian Safri Prakoso <berliansafrip@gmail.com>
2023-03-06 01:27:05 +0700

    add new

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("welcome praktikan KLS D")
+
+# dictionary
+name = {17: "andi", 18: "Rahmat", 19: "Rahman", 20: "upin"}
+
+# loop through dictionary
+for key, value in name.items():
+    print(f"hello {value} with id {key}")
+
+# adding dictionary from user input
+name[21] = input("masukkan nama: ")
+
+# print latest dictionary
+print(name)
