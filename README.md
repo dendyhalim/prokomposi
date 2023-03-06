e209ad0647e1d08e20ceb8a5961da302da59a9cb
Berlian Safri Prakoso <berliansafrip@gmail.com>
2023-03-06 10:03:16 +0700

    commit pertama

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # prokomposi
+
+## week 1
+pada pertemuan kali ini kita belajar git
+
+### week 2
+* belajar markdown
+* belajar python
+* belajar git
---
+++ safri.py
@@ -0,0 +1,13 @@
+nama_depan = "Safri"
+nama_blk = "berlian"
+
+angka = 10
+desimal = 0.5
+boolean = True
+kelas_D = ["safri", "ksl d", "dll"]
+data = {"nama": "safri", "kelas": "KLS D", "nim": 123}
+
+print(nama_depan)
+print("nama saya adalah safri")
+
+print(f"nama saya adalah {nama_depan} {nama_blk} dari kelas {data['kelas']}")
